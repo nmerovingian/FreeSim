2f9cf389359179076f91382a3e852af3a554f707
nmerovingian <nmerovingian@gmail.com>
2022-04-29 12:25:37 +0100

    MH theory

--- helper.py
+++ helper.py
@@ -1,5 +1,8 @@
+from errno import EPIPE
 from PyQt5.QtWidgets import QAction, QBoxLayout, QComboBox, QFormLayout, QGroupBox, QLabel, QListWidget, QMessageBox, QProgressBar, QWidget,QLineEdit,QCheckBox,QRadioButton,QMainWindow,QVBoxLayout,QHBoxLayout,QGridLayout,QPushButton,QApplication,QFileDialog,QGroupBox,QButtonGroup,QToolBar,QTabWidget
 import math
+from scipy.integrate import quad
+import numpy as np
 
 def getValue(widget):
     if isinstance(widget,QLineEdit):
@@ -77,3 +80,55 @@ def toDimensionalCA(time,fluxes,geometry,dElectrode,lElectrode,E0f,Temperature,D
         raise ValueError
 
     return time,fluxes
+
+
+def calMHInt(theta,Lambda,gamma,red=True):
+    """
+    Lambda:Dimensionless reorgnization energy
+    theta: dimensionless overpotential 
+    gamma: asymmetric parameter taking value between -1 and 1
+    """
+    if red:
+        def intexp(epsilon):
+            term1 = Lambda/4 * (1 + (theta+epsilon)/Lambda) ** 2
+            term2 = gamma*(theta+epsilon)/4 * (1-((theta+epsilon)/Lambda)**2)
+            term3 = Lambda/16*gamma*gamma
+            deltaG = term1+ term2 + term3
+            return np.exp(-deltaG)/(1+np.exp(-epsilon)) 
+
+    else:
+        def intexp(epsilon):
+            term1 = Lambda/4 * (1 - (theta+epsilon)/Lambda) ** 2
+            term2 = gamma*(theta+epsilon)/4 * (1-((theta+epsilon)/Lambda)**2)
+            term3 = Lambda/16*gamma*gamma
+            deltaG = term1+ term2 + term3
+            return np.exp(-deltaG)/(1+np.exp(+epsilon)) 
+
+    return quad(intexp,-50,50)[0]
+
+
+
+def calMH(K0,theta,Lambda,gamma):
+    """
+    Calculate rate constants using asymmetric Macus-Hush theory
+    K0: dimensionless electrochemical rate constant
+    theta: dimensionless overpotential 
+    Lambda:Dimensionless reorgnization energy
+    gamma: asymmetric parameter taking value between -1 and 1. Whe gamma == 0, it is the well-known symmetric MH equation
+    """
+
+    # The dimensionless activation energy of the reduction/oxidation processes in the asymmertic version of MH is calculated as: 
+    IRed = calMHInt(theta,Lambda,gamma,red=True)
+    IRedBase = calMHInt(0.0,Lambda,gamma,red=True)
+    IOx = calMHInt(theta,Lambda,gamma,red=False)
+    IOxBase = calMHInt(0.0,Lambda,gamma,red=False)
+
+    Kred = K0 * IRed/IRedBase
+    Kox = K0 * IOx/IOxBase
+    return Kred,Kox
+
+
+
+
+    
+
