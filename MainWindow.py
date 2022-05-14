1a4d23afeb6c5bc2c19f98ca4b3b2c6936d4a361
nmerovingian <nmerovingian@gmail.com>
2022-05-14 12:09:17 +0100

    Update MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -81,7 +81,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle('FreeSim, Open-Source Electrochemical Reaction Simulator V 0.0')
+        self.setWindowTitle('FreeSim, Open-Source Electrochemical Reaction Simulator V1.0')
 
         self.layout = QVBoxLayout()
 
